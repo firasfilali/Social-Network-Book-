d28279bdb07bc63921e8467dd288ee48b65b323e
firasfilali <76403765+firasfilali@users.noreply.github.com>
2024-07-02 20:25:01 +0200

    Complete JWT service

--- book-network-api/src/main/java/com/filali/book/security/JwtFilter.java
+++ book-network-api/src/main/java/com/filali/book/security/JwtFilter.java
@@ -4,21 +4,40 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.validation.constraints.NotNull;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+@RequiredArgsConstructor
 @Service
 public class JwtFilter extends OncePerRequestFilter {
+
+    private final Jwtservice jwtService;
+
     @Override
-    protected void doFilterInternal(
-            @NonNull HttpServletRequest request,
-            @NonNull HttpServletResponse response,
-            @NonNull FilterChain filterChain
+    protected void doFilterInternal( // every time we have a request this method will be executed
+                                     @NonNull HttpServletRequest request,
+                                     @NonNull HttpServletResponse response,
+                                     @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
+        if (request.getServletPath().contains("/api/v1/auth")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        final String authHeader = request.getHeader(AUTHORIZATION);
+        final String jwt;
+        final String userEmail; // the user email that will extract from our token
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) { //this is not the token that we are looking for
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(jwt);
 
     }
 }
